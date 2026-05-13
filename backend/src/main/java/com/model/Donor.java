52fe2582b615ec72bc9a0a95de71207b0a29852b
Vikash Sharma <vikashsharma1228@gmail.com>
2026-05-13 14:53:48 +0530

    Fix: Added missing Lombok imports for constructors

--- backend/src/main/java/com/model/Donor.java
+++ backend/src/main/java/com/model/Donor.java
@@ -3,6 +3,8 @@ package com.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+import lombok.NoArgsConstructor;  // <--- Yeh line add karein
+import lombok.AllArgsConstructor;
 
 
 @Data 
